cb56d38d75251193160bcf214af84662c3de68a2
ParthMishra <paarthmishra1@gmail.com>
2025-03-12 20:13:47 +0530

    PostgresDB Connection

--- src/main/java/com/example/demo/dao/PersonDataAccessService.java
+++ src/main/java/com/example/demo/dao/PersonDataAccessService.java
@@ -1,8 +1,8 @@
 package com.example.demo.dao;
 
 import com.example.demo.model.Person;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 import java.util.Optional;
@@ -10,6 +10,12 @@ import java.util.UUID;
 
 @Repository("posgress")
 public class PersonDataAccessService implements PersonDao{
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public PersonDataAccessService(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
 
     @Override
     public int insertPerson(UUID id, Person person) {
@@ -23,7 +29,12 @@ public class PersonDataAccessService implements PersonDao{
 
     @Override
     public List<Person> selectAllPeople() {
-        return List.of(new Person(UUID.randomUUID(),"From posgress db"));
+        final String sql = "Select id,name from person";
+        return jdbcTemplate.query(sql,(resultSet,i)->{
+                UUID id = UUID.fromString( resultSet.getString("id"));
+                String name = resultSet.getString("name");
+            return new Person(id ,name);
+        });
     }
 
     @Override
